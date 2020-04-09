857d27a28b53b2495dc9e3b835c9762480413718
Cole <coleabrusci@gmail.com>
2020-04-09 12:55:40 -0700

    made code more readable and more intuitive

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -6,53 +6,49 @@ def show_game_field(cells: str):
     print(head_str)
 
 
-def get_game_sets():
-    global cells_list
-    cells_list = [char for char in state]
-    sets = [cells_list[6:], cells_list[3:6], cells_list[:3],        # rows
-            cells_list[6::-3], cells_list[7::-3], cells_list[8::-3],    # columns
-            cells_list[::4], cells_list[2:7:2]]                     # diagonals
+def get_game_sets(game_state: str):
+    cells_list = [char for char in game_state]
+    sets = [cells_list[:3], cells_list[3:6], cells_list[6:],            # rows
+            cells_list[::3], cells_list[1::3], cells_list[2::3],    # columns
+            cells_list[::4], cells_list[2:7:2]]                         # diagonals
     return sets
 
 
 def current_game_state():
     global game_over
+    game_over = True
     finished = True
     x_wins = False
     o_wins = False
-    sets = get_game_sets()
+    sets = get_game_sets(state)
 
-    for _ in sets:
-        if all(cell == _[0] for cell in _):
-            if _[0] == 'X':
+    for item in sets:
+        if all(cell == item[0] for cell in item):
+            if item[0] == 'X':
                 x_wins = True
-            elif _[0] == 'O':
+            elif item[0] == 'O':
                 o_wins = True
             else:
                 pass
 
     if not x_wins and not o_wins:
-        for item in sets:
-            if '_' in item:
-                finished = False
-        if finished:
-            print('Draw')
-            game_over = True
-    elif x_wins:
+        for char in state:
+            finished = False if '_' in char else True
+
+    if x_wins:
         print('X wins')
-        game_over = True
     elif o_wins:
         print('O wins')
-        game_over = True
+    elif finished:
+        print('Draw')
     else:
-        pass
+        game_over = False
 
 
 def make_move():
-    columns = get_game_sets()[3:6]
+    rows = get_game_sets(state)[:3]
     while True:
-        user_move = input('Enter the coordinates: ')
-        coords = user_move.split()
+        coords = input('Enter the coordinates: ').split()
 
         try:
             x_coord = int(coords[0]) - 1
@@ -63,26 +59,22 @@ def make_move():
 
         if not 0 <= x_coord <= 2 or not 0 <= y_coord <= 2:
             print('Coordinates should be from 1 to 3!')
-        elif columns[x_coord][y_coord] != '_':
+        elif rows[x_coord][y_coord] != '_':
             print('This cell is occupied! Choose another one!')
         else:
             if state.count('X') == state.count('O'):
-                columns[x_coord][y_coord] = 'X'
+                rows[x_coord][y_coord] = 'X'
             else:
-                columns[x_coord][y_coord] = 'O'
+                rows[x_coord][y_coord] = 'O'
             new_state = ''
-            temp = ''
-            for x in columns:
+            for x in rows:
                 for y in x:
-                    temp += y
-            for i in range(3):
-                new_state += temp[2-i::3]
+                    new_state += y
             return new_state
 
 
 state = '_________'
 game_over = False
-cells_list = []
 show_game_field(state)
 while not game_over:
     state = make_move()
